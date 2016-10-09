5d16872f47e90c4643041231de803739d68b964e
rowana16 <aaron.rowan@gmail.com>
2016-10-09 19:42:22 -0400

    Initial Commit

---
+++ MVVMLearningData/TrainingProductManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLearningData
+{
+    public class TrainingProductManager
+    {
+        public List<TrainingProduct> Get()
+        {
+            List<TrainingProduct> ret = new List<TrainingProduct>();
+            ret = CreateMockData();
+
+            // TODO: Add you Data Access Method here
+            return ret;
+
+        }
+        private List<TrainingProduct> CreateMockData()
+        {
+            List<TrainingProduct> ret = new List<TrainingProduct>();
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 1,
+                ProductName = "Extending Bootstrap with CSS, javaScript and jQuery",
+                IntroductionDate = Convert.ToDateTime("10/1/2016"),
+                Url = "",
+                Price = Convert.ToDecimal(29)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 2,
+                ProductName = "C# for Everything",
+                IntroductionDate = Convert.ToDateTime("9/1/2016"),
+                Url = "",
+                Price = Convert.ToDecimal(29)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 3,
+                ProductName = "Lamp Stack Ahoy",
+                IntroductionDate = Convert.ToDateTime("6/1/2016"),
+                Url = "",
+                Price = Convert.ToDecimal(29)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 4,
+                ProductName = "Mean Stack is for Losers",
+                IntroductionDate = Convert.ToDateTime("3/1/2016"),
+                Url = "",
+                Price = Convert.ToDecimal(29)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 5,
+                ProductName = "Cold Fusion is THE FUTURE",
+                IntroductionDate = Convert.ToDateTime("10/1/2010"),
+                Url = "",
+                Price = Convert.ToDecimal(29)
+            });
+
+            return ret;
+        }
+    }
+}
---
+++ MVVMLearningData/TrainingProductViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLearningData
+{
+    public class TrainingProductViewModel
+    {
+        public List<TrainingProduct> Products { get; set; }
+        public string EventCommand { get; set; }
+
+        public TrainingProductViewModel()
+        {
+            Products = new List<TrainingProduct>();
+            EventCommand = "List";
+
+        }
+       
+        public void HandleRequest()
+        {
+            switch (EventCommand.ToLower())
+            {
+                case "list":
+                    Get(); // populate the Products property
+                    break;
+
+                case "add":
+                    Get(); // populate the Products property
+                    break;
+
+                case "edit":
+                    Get(); // populate the Products property
+                    break;
+
+
+
+                default:
+                    break;
+            }
+        }
+
+
+        private void Get()
+        {
+            TrainingProductManager mgr = new TrainingProductManager();
+            Products = mgr.Get();
+
+        }
+    }
+}
